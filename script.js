b26bc3d4c218b66b9974de653d8e3824e7ed54ff
Fredrik Isehag <fredrikisehag@gmail.com>
2022-12-12 20:42:32 +0100

    Add comments to function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Array of the different choices for computer.
 const choiceArray = ["Rock", "Paper", "Scissor"];
 
+//Function that returns a random computer choice.
 function getComputerChoice() {
   return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
