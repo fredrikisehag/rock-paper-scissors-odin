63789a19636f9d1bb888050048c46f3380b88fb7
Fredrik Isehag <fredrikisehag@gmail.com>
2022-12-13 20:36:01 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 //Array storing computers different choices:
-const choiceArray = ["rock", "paper", "scissor"];
+const choiceArray = ["rock", "paper", "scissors"];
 
 //Function that randomly selects choice for computer:
 function getComputerChoice() {
   return choiceArray[Math.floor(Math.random() * choiceArray.length)];
+}
+
+//Function that plays one round of Rock Paper Scissors and declares winner of round:
+function playRound(playerSelection, computerSelection) {
+  //If player wins:
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You won! Rock beats Scissors!";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You won! Scissors beats Paper!";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You won! Paper beats Rock!";
+    //If computer wins:
+  } else if (computerSelection == "rock" && playerSelection == "scissors") {
+    return "You lost! Rock beats Scissors!";
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    return "You lost! Scissors beats Paper!";
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    return " You lost! Paper beats Rock!";
+  }
 }
