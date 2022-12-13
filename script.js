acb608ba7026ee50f2282a79f152f1a3a879d230
Fredrik Isehag <fredrikisehag@gmail.com>
2022-12-13 20:25:38 +0100

    Add choiceArray and getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+//Array storing computers different choices:
+const choiceArray = ["rock", "paper", "scissor"];
 
+//Function that randomly selects choice for computer:
+function getComputerChoice() {
+  return choiceArray[Math.floor(Math.random() * choiceArray.length)];
+}
